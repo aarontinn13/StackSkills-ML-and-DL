62d0bcd6566c08c76f26ae61a876564ba84a42c5
Aaron <tinn.3@buckeyemail.osu.edu>
2018-10-04 22:18:29 -0500

    initial push of all data

---
+++ Python Files/SimpleMath.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import tensorflow as tf
+import tensorboard as tb
+
+
+
+# In[ ]:
+
+a = tf.constant(6.5, name='constant_a') #create constant values
+b = tf.constant(3.4, name='constant_b')
+c = tf.constant(3.0, name='constant_c')
+d = tf.constant(100.2, name='constant_d')
+
+
+# In[ ]:
+
+add = tf.add(a, b, name="add_ab")
+subtract = tf.subtract(b, c, name="subtract_bc")
+square = tf.square(d, name="square_d")
+
+
+# In[ ]:
+
+final_sum = tf.add_n([add, subtract, square], name="final_sum")
+
+
+# In[ ]:
+
+with tf.Session() as sess:
+
+    print("a + b: ", sess.run(add))
+    print("b - c: ", sess.run(subtract))
+    print("Square of d: ", sess.run(square))
+    
+    print("Final sum", sess.run(final_sum))
+    
+    another_sum = tf.add_n([a, b, c, d, square], name="another_sum")
+    print("Another sum ", sess.run(another_sum))
+    
+
+
+# In[ ]:
+
+writer = tf.summary.FileWriter('../SimpleMath', sess.graph)
+writer.close()
+
+tb.summary()
---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# StackSkills-Machine-Learning
+ML and DL tutorial on stack skills with data sets and modules
+=======
+# StackSkills-ML-and-DL
+resource for practicing Machine and Deep Learning from StackSkills
+>>>>>>> da55b7f0a2381a2354a8ebb858021e1a2dc53d58
